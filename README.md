f14870a63a96ee2e30c5542198ca463fbc89a2ea
Albari Islam <albari27@gmail.com>
2019-11-19 22:31:52 -0330

    Added html, css, js files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Checkers-Game
+
+* This game/application is the final project for COMP 3200 Course
+* Group members: Atul Mehla, Albari Islam, and Aditi Joshi
---
+++ objects.js
@@ -0,0 +1,13 @@
+class Player
+{
+    constructor(playerNum, score)
+    {
+        this.playerNum = playerNum;
+        this.score = score;
+    }
+
+    getScore()
+    {
+        return this.score;
+    }
+}
