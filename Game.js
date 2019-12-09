98d198fb3630fed6a3eba08fe3b79e9c933cb515
truedem0n <atulmehlasmart@gmail.com>
2019-12-09 01:24:41 -0330

    Checkers complete

--- Game.js
+++ Game.js
@@ -1,20 +1,34 @@
+// Caching the dom
 const board = document.getElementById("board")
 const redScore = document.getElementById("redScore")
 const blackScore = document.getElementById("blackScore")
 const playerTurn = document.getElementById("player")
+
+// fix for different screen size
 const size = parseInt((window.innerHeight * 0.8) / 8)
+
+
 const playingWith = {
-    "Opponent (Red)": "human"
-}
+        "Opponent (Red)": ""
+    },
+    config = {
+        "Time limit(ms)": 1000,
+        "maxDepth": 0,
+        player: 1
+    }
+
+
 const dgui = new dat.GUI();
 
 
 class Game {
     constructor(playerNum, score) {
+
+        this.AI = null;
         // state board
         this.boardState = new BoardState()
         this.selected = [null, null]
-            // gui board, only contains the  circles
+        // gui board, only contains the  circles
         this.guiState = this.twoDArray(8)
 
         // contains all the rectangles
@@ -31,7 +45,9 @@ class Game {
 
     setUpController() {
         let opp = dgui.add(playingWith, "Opponent (Red)", ['Human', 'AI']);
-        opp.setValue("Human")
+        opp.setValue("AI")
+        dgui.add(config, "Time limit(ms)", 0)
+        dgui.add(config, "maxDepth", 0)
     }
 
     twoDArray(x) {
@@ -55,11 +71,11 @@ class Game {
                 // the board rectangles
                 let rec = t.makeRectangle(i * size + size / 1.5, j * size + size / 1.5, size, size);
                 if ((i % 2 != 0 && j % 2 == 0) || (i % 2 == 0 && j % 2 != 0))
-                    rec.fill = "rgba(100,100,100,1)"
-                else if(!(i % 2 != 0 && j % 2 == 0) || (i % 2 == 0 && j % 2 != 0)){
-                    rec.fill = "rgba(100,100,0,1)"
+                    rec.fill = "rgba(0,0,0,1)"
+                else if (!(i % 2 != 0 && j % 2 == 0) || (i % 2 == 0 && j % 2 != 0)) {
+                    rec.fill = "rgba(255,255,255,1)"
                 }
-                    
+
                 checkersBoardRow.push(rec);
 
                 // player circles
@@ -109,7 +125,15 @@ class Game {
                 this.checkersBoard[i][j]._renderer.elem.onclick = (e) => {
                     let x = e.target.getAttribute("pos")[0],
                         y = e.target.getAttribute("pos")[2]
-                    if (this.selected[0] != null && this.boardState.isMoveLegal({ x: this.selected[0], y: this.selected[1] }, { x: y, y: x }, this)) {
+
+                    // Human move start
+                    if (this.selected[0] != null && BoardState.isMoveLegal({
+                            x: this.selected[0],
+                            y: this.selected[1]
+                        }, {
+                            x: y,
+                            y: x
+                        }, this, this.boardState.getBoard())) {
                         let t = this.two;
                         let boardState = this.boardState.getBoard();
                         this.guiState[this.selected[0]][this.selected[1]].translation.set(x * size + size / 1.5, y * size + size / 1.5)
@@ -117,10 +141,23 @@ class Game {
                         this.guiState[y][x] = Object.assign(t.makeCircle(), this.guiState[this.selected[0]][this.selected[1]])
                         boardState[y][x] = boardState[this.selected[0]][this.selected[1]]
                         boardState[this.selected[0]][this.selected[1]] = 0
+                        // console.log("human  move: ", y, x, x * size + size / 1.5, y * size + size / 1.5)
                         this.guiState[y][x]._renderer.elem.setAttribute("pos", `${y}_${x}`)
                         this.guiState[this.selected[0]][this.selected[1]] = 0
+                        if (y == 0 || y == 7) {
+                            this.setKing([y, x], boardState[y][x])
+                        }
                         this.selected = [null, null]
+                        //Human move complete
+
+                        // if playing with AI
+                        if (playingWith["Opponent (Red)"] == "AI") {
+                            if (this.AI == null)
+                                this.AI = new AI(config)
+                            this.makeComputerMove(this.AI.getAction(this.boardState))
+                        }
                         this.updateScore()
+
                     }
                     this.two.update()
                 }
@@ -130,16 +167,58 @@ class Game {
         this.two.update();
     }
 
+
+    makeComputerMove(action) {
+        if(action===null)
+            return
+        let pX = action[0].x,
+            pY = action[0].y,
+            nX = action[1].x,
+            nY = action[1].y,
+            boardState = this.boardState.getBoard();
+        // console.log(action)
+        BoardState.isMoveLegal({
+            x: pX,
+            y: pY
+        }, {
+            x: nX,
+            y: nY
+        }, this, this.boardState.getBoard())
+        this.guiState[pX][pY].translation.set(parseInt(nY * size + size / 1.5), parseInt(nX * size + size / 1.5))
+        this.guiState[nX][nY] = Object.assign(this.two.makeCircle(), this.guiState[pX][pY])
+        boardState[nX][nY] = boardState[pX][pY]
+        boardState[pX][pY] = 0
+        this.guiState[nX][nY]._renderer.elem.setAttribute("pos", `${nX}_${nY}`)
+        this.guiState[pX][pY] = 0
+        if (nX == 0 || nX == 7) {
+            this.setKing([nX, nY], 1)
+        }
+        
+
+    }
+
     setKing(coord, player) {
+        let board=this.boardState.getBoard()
+        if (board[coord[0]][coord[1]] == red) {
+            board[coord[0]][coord[1]] = 1.1
+        } else if (board[coord[0]][coord[1]] == black) {
+            board[coord[0]][coord[1]] = -1.1
+        }
+        // console.log(player)
         if (player == 1)
-            this.guiState[coord[0]][coord[1]].fill = "rgb(150,0,0)"
+            this.guiState[coord[0]][coord[1]].fill = "#B55"
         else if (player == -1)
-            this.guiState[coord[0]][coord[1]].fill = "rgb(150,150,150)"
+            this.guiState[coord[0]][coord[1]].fill = "#999"
+        // this.two.update();
     }
 
     removeMiddlePiece(coord) {
+        // console.log("remove piece", coord)
+        document.getElementById(this.guiState[coord[0]][coord[1]].id).remove();
         this.guiState[coord[0]][coord[1]].remove();
-        this.boardState.getBoard()[coord[0]][coord[1]] = 0
+        this.guiState[coord[0]][coord[1]] = 0;
+        // this.boardState.getBoard()[coord[0]][coord[1]] = 0
+        this.two.update();
     }
     updateScore() {
         let board = this.boardState.getBoard(),
@@ -149,13 +228,13 @@ class Game {
             for (let j = 0; j < board[i].length; j++) {
                 if (parseInt(board[i][j]) == 1)
                     red++
-                    else if (parseInt(board[i][j]) == -1) black++
+                else if (parseInt(board[i][j]) == -1) black++
             }
         redScore.innerHTML = red
         blackScore.innerHTML = black
-        if (parseInt(this.boardState.getTurn()) == 1)
-            playerTurn.innerHTML = "RED"
-        else playerTurn.innerHTML = "BLACK"
+        // if (parseInt(this.boardState.getTurn()) == 1)
+        //     playerTurn.innerHTML = "RED"
+        // else playerTurn.innerHTML = "BLACK"
         if (red == 0)
             playerTurn.innerHTML = "Black wins"
         else if (black == 0)
